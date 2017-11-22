a7b1561b702c355b27f39d43b54f5ab4b12197f2
Vincent-094 <32720536+Vincent-094@users.noreply.github.com>
2017-11-22 15:01:44 -0800

    Update account_run.py

--- account_run.py
+++ account_run.py
@@ -9,7 +9,7 @@ def main():
 
 
 #no interest, and no minimum balance
-    SC_checking_object = accounts.ServiceChargeChecking(1, 1000, 0, 0)
+    SC_checking_object = accounts.ServiceChargeChecking(1, 1000, 0, 0, 5)
 #interest, checks, min balance
     NSC_checking_object = accounts.NoServiceChargeChecking(2, 1000, 1, 100)
 #high interest and higher min balance.
@@ -22,58 +22,55 @@ def main():
 #it has instance variables to store the number of CD maturity months, interest rate, and the current CD month.
     CD_object = accounts.CertificateOfDeposit(6, 1000, 5, 0, 60, 30)
 
-    action = menu()
-    account = submenu()
-    amount = int(input('What is the amount? '))
-    if action == 1 and account == 1:
-        money(action,amount, SC_checking_object)
-    elif action == 2 and account == 1:
-        money(action,amount, SC_checking_object)
-    elif action == 3 and account == 1:
-        counter = 0
-        while counter <= 5:
-            if counter > 5:
-                print('You have exceeded the check writing limits for this month.')
-            counter += 1
+    answer = 'y'
+    while answer == 'y' or answer == 'Y':
+        action = menu()
+        account = submenu()
+        amount = int(input('What is the amount? '))
+        if action == 1 and account == 1:
             money(action,amount, SC_checking_object)
-            action = menu()
+        elif action == 2 and account == 1:
+            money(action,amount, SC_checking_object)
+        elif action == 3 and account == 1:
+            money(action,amount, SC_checking_object)
 
-    elif action == 1 and account == 2:
-        money(action, amount, NSC_checking_object)
-    elif action == 2 and account == 2:
-        money(action,amount, NSC_checking_object)
-    elif action == 3 and account == 2:
-        money(action,amount, NSC_checking_object)
+        elif action == 1 and account == 2:
+            money(action, amount, NSC_checking_object)
+        elif action == 2 and account == 2:
+            money(action,amount, NSC_checking_object)
+        elif action == 3 and account == 2:
+            money(action,amount, NSC_checking_object)
 
-    elif action == 1 and account == 3:
-        money(action, amount, HighInterest_checking_object)
-    elif action == 2 and account == 3:
-        money(action,amount, HighInterest_checking_object)
-    elif action == 3 and account == 3:
-        money(action,amount, HighInterest_checking_object)
+        elif action == 1 and account == 3:
+            money(action, amount, HighInterest_checking_object)
+        elif action == 2 and account == 3:
+            money(action,amount, HighInterest_checking_object)
+        elif action == 3 and account == 3:
+            money(action,amount, HighInterest_checking_object)
 
-    elif action == 1 and account == 4:
-        money(action, amount, Savings_object)
-    elif action == 2 and account == 4:
-        money(action,amount, Savings_object)
+        elif action == 1 and account == 4:
+            money(action, amount, Savings_object)
+        elif action == 2 and account == 4:
+            money(action,amount, Savings_object)
 
-    elif action == 1 and account == 5:
-        money(action, amount, High_savings_object)
-    elif action == 2 and account == 5:
-        money(action,amount, High_savings_object)
+        elif action == 1 and account == 5:
+            money(action, amount, High_savings_object)
+        elif action == 2 and account == 5:
+            money(action,amount, High_savings_object)
 
-    elif action == 1 and account == 6:
-        money(action, amount, CD_object)
-    elif action == 2 and account == 6:
-        money(action,amount, CD_object)
+        elif action == 1 and account == 6:
+            money(action, amount, CD_object)
+        elif action == 2 and account == 6:
+            money(action,amount, CD_object)
 
-    print()
-    print(SC_checking_object)
-    print(NSC_checking_object)
-    print(HighInterest_checking_object)
-    print(Savings_object)
-    print(High_savings_object)
-    print(CD_object)
-    print()
-
+        print()
+        print(SC_checking_object)
+        print(NSC_checking_object)
+        print(HighInterest_checking_object)
+        print(Savings_object)
+        print(High_savings_object)
+        print(CD_object)
+        print()
+        answer = input('Do you want to continue y/n? ')
+        priont
 main()
